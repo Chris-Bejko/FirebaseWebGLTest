b7ae976003e2042c1987288480d5c8780a36619a
Chris Bejko <92367999+Chris-Bejko@users.noreply.github.com>
2022-05-13 15:16:49 +0300

    Working Product
    Check it out

--- Assets/Plugins/firebase.jslib
+++ Assets/Plugins/firebase.jslib
@@ -1,7 +1,57 @@
 mergeInto(LibraryManager.library, {
 
-  Hello: function () {
-    window.alert("Hello, world!");
-  },
+  GetJSON: function (path, objectName, callback, fallback) {
+		var parsedPath = Pointer_stringify(path);
+		var parsedObjectName= Pointer_stringify(objectName);
+		var parsedCallback = Pointer_stringify(callback);
+		var parsedFallback = Pointer_stringify(fallback);
+
+		try{
+		firebase.database().ref(parsedPath).once('value').then(function(snapshot) {
+			window.unityInstance.SendMessage(parsedObjectName, parsedCallback , JSON.stringify(snapshot.val()));
+
+
+		});
+
+		} catch(error){
+			window.unityInstance.SendMessage(parsedObjectName, parsedFallback, "There was an error: " + error.message);
+			
+		}
+		},
+
+	  PushJSON: function(path, value, objectName, callback, fallback) {
+        var parsedPath = UTF8ToString(path);
+        var parsedValue = UTF8ToString(value);
+        var parsedObjectName = UTF8ToString(objectName);
+        var parsedCallback = UTF8ToString(callback);
+        var parsedFallback = UTF8ToString(fallback);
+
+        try {
+
+            firebase.database().ref(parsedPath).push().set(parsedValue).then(function(unused) {
+                window.unityInstance.SendMessage(parsedObjectName, parsedCallback, "Success: " + parsedValue + " was pushed to " + parsedPath);
+            });
+
+        } catch (error) {
+            window.unityInstance.SendMessage(parsedObjectName, parsedFallback, JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        }
+    },
+	PostJSON: function(path, value, objectName, callback, fallback) {
+        var parsedPath = UTF8ToString(path);
+        var parsedValue = UTF8ToString(value);
+        var parsedObjectName = UTF8ToString(objectName);
+        var parsedCallback = UTF8ToString(callback);
+        var parsedFallback = UTF8ToString(fallback);
+
+        try {
+
+            firebase.database().ref(parsedPath).set(parsedValue).then(function(unused) {
+                unityInstance.Module.SendMessage(parsedObjectName, parsedCallback, "Success: " + parsedValue + " was posted to " + parsedPath);
+            });
+
+        } catch (error) {
+            unityInstance.Module.SendMessage(parsedObjectName, parsedFallback, JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        }
+    },
 
 });
--- Assets/Scripts/FirebaseInit.cs
+++ Assets/Scripts/FirebaseInit.cs
@@ -1,52 +1,53 @@
 using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FirebaseInit : MonoBehaviour
 {
-     public Text text;
-
-    /* [DllImport("_Internal")]
-     private static extern void GetJSON(string path, string objectName, string callback, string fallback);
-
-
-     void Start()
-     {
-         text.text = "Start worked";
-         GetJSON(path: "example", gameObject.name, callback: "OnRequestSuccess", fallback: "OnRequestFailed");
-     }
-
-     public  void OnRequestSuccess(string data)
-     {
-         text.color = Color.green;
-         text.text = data;
-     }
-
-     public void OnRequestFailed(string error)
-     {
-         text.color = Color.red;
-         text.text = error;
-     }
-
-     public void MyFunction()
-     {
-         text.text = "My function success";
-     }
-    */
-
+    public Text text;
+    public static FirebaseInit Instance { get; private set; }
     [DllImport("__Internal")]
-    private static extern void Hello();
-
-    private void Start()
+    private static extern void GetJSON(string path, string objectName, string callback, string fallback);
+    [DllImport("__Internal")]
+    private static extern void PushJSON(string path, string value, string objectName, string callback, string fallback);
+    [DllImport("__Internal")]
+    private static extern void PostJSON(string path, string value, string objectName, string callback, string fallback);
+    IEnumerator Start()
     {
-        Hello();
+        text.text = "Start worked";
+        GetJSON(path: "example", gameObject.name, callback: "OnRequestSuccess", fallback: "OnRequestFailed");
+        yield return new WaitForSeconds(2f);
+        PushJSON(path: "example1", value: "value1", objectName: gameObject.name, callback: "OnRequestSuccess", fallback: "OnRequestFailed");
+        yield return new WaitForSeconds(2f);
+        PostJSON(path: "example2", value: "value2", objectName: gameObject.name, callback: "OnRequestSuccess", fallback: "OnRequestFailed");
     }
 
+    public void OnRequestSuccess(string data)
+    {
+        text.color = Color.green;
+        text.text = data;
+    }
+
+    public void OnRequestFailed(string error)
+    {
+        text.color = Color.red;
+        text.text = error;
+    }
+
+    public void MyFunction()
+    {
+        text.text = "My function success";
+    }
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     public void MessageFromJavascript(string message)
     {
         Debug.Log(message);
-        text.text = "Text Changed Successfully";
+        text.text = "Text Changed Successfully to " + message;
     }
 }
